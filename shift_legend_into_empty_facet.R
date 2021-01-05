3203d7d5240a002ffd785b88157ae33adb52e44d
NeilHobbs <neilphobbs@hotmail.com>
2021-01-05 11:28:36 +0000

    Function written by Artem Sokolov (stackoverflow)

---
+++ shift_legend_into_empty_facet.R
@@ -0,0 +1,12 @@
+#https://stackoverflow.com/questions/54438495/shift-legend-into-empty-facets-of-a-faceted-plot-in-ggplot2
+#Function written by Artem Sokolov
+shift_legend_into_empty_facet = function(p) {
+  pnls = cowplot::plot_to_gtable(p) %>% 
+    gtable::gtable_filter("panel") %>%
+    with(setNames(grobs, layout$name)) %>% 
+    purrr::keep(~identical(.x,zeroGrob()))
+  
+  if( length(pnls) == 0 ) stop( "No empty facets in the plot" )
+  
+  lemon::reposition_legend( p, "center", panel=names(pnls) )
+}
