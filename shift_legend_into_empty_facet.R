f56f6e13e71e5b856f31bc7108abb11ab74f603a
NeilHobbs <neilphobbs@hotmail.com>
2021-01-05 11:35:04 +0000

    improved naming

--- shift_legend_into_empty_facet.R
+++ shift_legend_into_empty_facet.R
@@ -1,7 +1,7 @@
 #https://stackoverflow.com/questions/54438495/shift-legend-into-empty-facets-of-a-faceted-plot-in-ggplot2
 #Function written by Artem Sokolov
-shift_legend_into_empty_facet = function(p) {
-  pnls = cowplot::plot_to_gtable(p) %>% 
+shift_legend_into_empty_facet = function(your.graph) {
+  pnls = cowplot::plot_to_gtable(your.graph) %>% 
     gtable::gtable_filter("panel") %>%
     with(setNames(grobs, layout$name)) %>% 
     purrr::keep(~identical(.x,zeroGrob()))
