8c207b30c7a17014ec065a7d43cb7516bb0ca1b0
NeilHobbs <64196425+NeilHobbs@users.noreply.github.com>
2024-06-03 16:41:58 +0200

    Create install_list_R_packages.R
    useful for when setting up on new computers

---
+++ install_list_R_packages.R
@@ -0,0 +1,54 @@
+#R Packages Installation Script
+#This is basically a list of R packages I generally use and it is basically just a case of
+  #installing them all as and when using a new device etc. And just a handy list to have.
+
+install.packages(c("tidyverse",  #dplyr tidyr ggplot2 stringr etc
+                   "patchwork",  #panels of plots
+                   "ggarrange",  #panels of plots etc
+                   "grid.Extra", #panelsof plots
+                   "ggfortify", #more plotting tools stuff
+                   "RColorBrewer",  #colours for plots
+                   "alluvial",  #making sankey style diagrams
+                   "GGally", #more plotting stuff
+                   "ggridges", #good for histograms with multiple factors
+                   "shiny", #for shiny applications
+                   "ggpubr",    #more plotting tools
+                   "sp", #spatial data
+                   "sf",   #spatial data
+                   "raster", #spatial data
+                   "rgeos", #spatial data
+                   "rgdal", #spatial data
+                   "maptools", #spatial data
+                   "tmap",  #plotting maps - easier than ggplot2 for maps I think
+                   "leaflet", #html maps
+                   "PrevMap", #geostatistics
+                   "geoR",  #spatial data
+                   "splancs",  #spatial data
+                   "rgdal", #spatial data
+                   "randomForest", #random forest models
+                   "caret", #classification trees
+                   "aer",  # for dispersion test
+                   "mgcv", #generalised additive models
+                   "lme4", #random effect models
+                   "parallel",  #parallelisations
+                   "foreach", # paralellisation
+                   "purrr", #paraleellisation
+                   "readxl", #read in data
+                   "epiR", #useful statistical tests
+                   "bayestestR", #useful statistical tests
+                   "MASS", #negative bionomial stuff
+                   "devtools", # R package making
+                   "testthat", # R package making
+                   "roxygen2", # R package making
+                   "hhh4",  #endemic-epidemic models
+                   "rpart", #regresssion classification trees
+                   "rpart.plot", # plotting rpart
+                   "performance", # regression model diagnostics
+                   "data.table", #lots of useful stuff
+                   "lhs", #random latin hypercube sampling
+                   "profvis", #profiling R functions and packages
+                   "ggforce", #more ggplot stuff eg zooming
+                   "gganimate", #ggplot animations
+                   "ggh4x" #more ggplot2 customisation
+
+                   ))
